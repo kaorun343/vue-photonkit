17b57a0859b4885e0d99c6341547976061adb697
kaorun343 <kaoru.h65536@live.jp>
2017-01-12 01:10:58 +0900

    update ListGroupItem

---
+++ play/ListGroup/ListGroup.play.js
@@ -0,0 +1,6 @@
+import { play } from 'vue-play'
+
+import basic from './basic.vue'
+
+play('List Group', module)
+  .add('basic', basic)
---
+++ play/ListGroup/basic.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <list-group title="Favorites">
+    <list-group-header>
+      <input class="form-control" type="text" placeholder="Search for someone">
+    </list-group-header>
+    <list-group-item v-for="n in 5" :active="n % 5 == 2" @click.native="clicked(n)">
+      <strong>{{ n }}. {{ title }}</strong>
+      <p>{{ text }}</p>
+    </list-group-item>
+  </list-group>
+</template>
+
+<script>
+import { ListGroup, ListGroupHeader, ListGroupItem, Icon } from '../../'
+
+export default {
+  data () {
+    return {
+      title: 'List item title',
+      text: 'Lorem ipsum dolor sit amet.'
+    }
+  },
+  methods: {
+    clicked (n) {
+      this.$log(`item ${n} clicked`)
+    }
+  },
+  components: { ListGroup, ListGroupHeader, ListGroupItem, Icon }
+}
+</script>
+
+<style lang="sass">
+@import '~photon/dist/css/photon.css'
+</style>
